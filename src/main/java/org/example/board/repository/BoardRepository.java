6e42becddcb18ed1cb08ca371879908f2d99919e
propose101101 <propose101101@gmail.com>
2025-04-03 13:04:16 +0900

    first commit

---
+++ src/main/java/org/example/board/repository/BoardRepository.java
@@ -0,0 +1,45 @@
+package org.example.board.repository;
+
+import lombok.RequiredArgsConstructor;
+import org.example.board.dto.BoardDTO;
+import org.example.board.dto.BoardFileDTO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class BoardRepository {
+    private final SqlSessionTemplate sql ;
+    public BoardDTO save(BoardDTO boardDTO) {
+        sql.insert("Board.save", boardDTO);
+        return boardDTO;
+    }
+
+    public List<BoardDTO> findAll() {
+        return sql.selectList("Board.findAll");
+    }
+
+    public void updateHits(long id) {
+        sql.update("Board.updateHits", id);
+    }
+
+    public BoardDTO findById(Long id) {
+        return sql.selectOne("Board.findById", id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        System.out.println("boardDTO = " + boardDTO);
+        sql.update("Board.update", boardDTO);
+    }
+
+
+    public void delete(long id) {
+        sql.delete("Board.delete", id);
+    }
+
+    public void saveFile(BoardFileDTO boardFileDTO) {
+        sql.insert("Board.saveFile", boardFileDTO);
+    }
+}
---
+++ src/main/java/org/example/board/service/BoardService.java
@@ -0,0 +1,66 @@
+package org.example.board.service;
+
+import lombok.RequiredArgsConstructor;
+import org.example.board.dto.BoardDTO;
+import org.example.board.dto.BoardFileDTO;
+import org.example.board.repository.BoardRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public void save(BoardDTO boardDTO) throws IOException {
+        if (boardDTO.getBoardFile() == null || boardDTO.getBoardFile().isEmpty()) {
+            boardDTO.setFileAttached(0);
+            boardRepository.save(boardDTO);
+        } else {
+            boardDTO.setFileAttached(1);
+            boardRepository.save(boardDTO);
+            BoardDTO savedBoard = boardRepository.save(boardDTO);
+
+            for (MultipartFile boardFile : boardDTO.getBoardFile()) {
+                String originalFilename = boardFile.getOriginalFilename();
+                String storedFileName = System.currentTimeMillis() + "-" + originalFilename;
+                String savePath = "C:/Temp/" + storedFileName;
+
+                boardFile.transferTo(new File(savePath));
+
+                BoardFileDTO boardFileDTO = new BoardFileDTO();
+                boardFileDTO.setOriginalFileName(originalFilename);
+                boardFileDTO.setStoredFileName(storedFileName);
+                boardFileDTO.setBoardId(savedBoard.getId());
+
+                boardRepository.saveFile(boardFileDTO);
+            }
+        }
+    }
+
+
+    public List<BoardDTO> findAll() {
+        return boardRepository.findAll();
+    }
+
+    public void updateHits(long id) {
+        boardRepository.updateHits(id);
+    }
+
+    public BoardDTO findById(long id) {
+        return boardRepository.findById(id);
+    }
+
+    public void update(BoardDTO boardDTO) {
+        boardRepository.update(boardDTO);
+    }
+
+
+    public void delete(long id) {
+        boardRepository.delete(id);
+    }
+}
